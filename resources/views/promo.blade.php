5ec02aa81a47790dd41ee5f7be6d460d061b2b00
NelsonEAX <nelsoneax@yandex.ru>
2017-11-26 21:40:33 +0500

    Yandex.Metrika counter

--- resources/views/promo.blade.php
+++ resources/views/promo.blade.php
@@ -205,7 +205,7 @@
                             </div>
                             <div class="pricing-box-body">
                                 <ul class="list-marked">
-                                    <li>Если заказ сделан с помощью нашей программы</li>
+                                    <li>Если потолки построены в нашей программе и отправлены через форму заказа на сайте</li>
                                 </ul>
                                 <a class="btn-block btn btn-default" data-toggle="modal" data-target="#programm-modal">Подробнее</a>
                             </div>
@@ -221,7 +221,7 @@
                             </div>
                             <div class="pricing-box-body">
                                 <ul class="list-marked">
-                                    <li>Заказ от 5000р</li>
+                                    <li>Заказ от 7000р</li>
                                 </ul>
                             </div>
                         </div>
@@ -697,5 +697,35 @@
     $('#programm-modal').modal('toggle');
 @endif
 </script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter46723851 = new Ya.Metrika({
+                    id:46723851,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+                    trackHash:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/46723851" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </body>
 </html>
